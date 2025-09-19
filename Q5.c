6b3ade1b62861e9a830b07328b89f7b409311755
Anirban chattaraj <anirbanchattaraj@Anirbans-MacBook-Air.local>
2025-09-19 23:03:28 +0530

    first commit

---
+++ Q5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+ int n;
+ printf("Enter your number:");
+ scanf("%d",&n);
+ int nst=2*n-1;
+ int nsp=0;
+ for (int i = 1; i <=n; i++)
+ {
+    for (int j = 1; j <=nsp; j++)
+    {
+        printf(" ");
+    }
+    for (int k = 1; k <=nst; k++)
+    {
+       printf("%d",k);
+    }
+    nst-=2;
+    nsp++;
+    printf("\n");
+ }
+ return 0;
+ 
+}
